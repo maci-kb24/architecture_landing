82de09cbd1cfde515f178ef1116c46ad34dfeb6d
Marjan Stojanovski <macikb8@yahoo.com>
2023-02-15 20:14:53 +0100

    create carousel slider

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+// mobile nav
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+//carousel slider
+document.addEventListener("DOMContentLoaded", function () {
+  const elms = document.getElementsByClassName("splide");
+
+  for (let i = 0; i < elms.length; i++) {
+    new Splide(elms[i]).mount();
+  }
+
+  //   var splide = new Splide(".splide");
+  //   splide.mount();
+});
